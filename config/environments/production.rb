1d5654cafa14e9620400c3458c2f602fa2d196fd
Adam Haris <iamadamharis@gmail.com>
2025-12-04 10:41:21 +0100

    configurable actionmailer settings (ENV)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,23 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = ENV.fetch("MAILER_DELIVERY_METHOD", "sendmail").to_sym
+
+  config.action_mailer.smtp_settings = {
+    address: ENV.fetch("SMTP_ADDRESS", "localhost"),
+    port: ENV.fetch("SMTP_PORT", "587").to_i,
+    domain: ENV.fetch("SMTP_DOMAIN", nil),
+    user_name: ENV.fetch("SMTP_USERNAME", nil),
+    password: ENV.fetch("SMTP_PASSWORD", nil),
+    authentication: ENV.fetch("SMTP_AUTHENTICATION", "plain"),
+    enable_starttls_auto: ENV.fetch("SMTP_ENABLE_STARTTLS_AUTO", "true") == "true"
+  }
+
+  config.action_mailer.sendmail_settings = {
+    location: ENV.fetch("SENDMAIL_LOCATION", "/usr/sbin/sendmail"),
+    arguments: ENV.fetch("SENDMAIL_ARGUMENTS", "-i")
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
